a871574a26731fb4dce09772f696775ca47e9db9
Nevio <nevio@eiger.co>
2024-06-23 10:30:42 +0200

    Beginning of GetBlocksInRange()

--- handlers_test.go
+++ handlers_test.go
@@ -51,3 +51,59 @@ func TestGetHeight(t *testing.T) {
 		})
 	}
 }
+
+func TestGetBlocksInRange(t *testing.T) {
+	testCases := []struct {
+		name      string
+		opts      options.Options
+		networkId utils.NetworkID
+		ranges    []struct {
+			start *big.Int
+			end   *big.Int
+		}
+	}{{
+		name: "Test Ethereum Client",
+		opts: options.Options{
+			Blockchains: []options.Node{
+				{
+					Type:        utils.EthereumNetwork,
+					NetworkId:   utils.EthereumNetworkID,
+					Endpoint:    "https://eth.hypersync.xyz",
+					RpcEndpoint: "https://eth.rpc.hypersync.xyz",
+				},
+			},
+		},
+		networkId: utils.EthereumNetworkID,
+		ranges: []struct {
+			start *big.Int
+			end   *big.Int
+		}{
+			{
+				start: big.NewInt(10000000),
+				end:   big.NewInt(10000002),
+			},
+		},
+	}}
+
+	for _, testCase := range testCases {
+		t.Run(testCase.name, func(t *testing.T) {
+			ctx, cancel := context.WithCancel(context.Background())
+			defer cancel()
+
+			hsClient, err := NewHyper(ctx, testCase.opts)
+			require.NoError(t, err)
+			require.NotNil(t, hsClient)
+
+			client, found := hsClient.GetClient(testCase.networkId)
+			require.True(t, found)
+			require.NotNil(t, client)
+
+			for _, r := range testCase.ranges {
+				ranges, rErr := client.GetBlocksInRange(ctx, r.start, r.end)
+				require.NoError(t, rErr)
+				require.NotNil(t, ranges)
+				utils.DumpNodeNoExit(ranges)
+			}
+		})
+	}
+}
